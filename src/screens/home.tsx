bdb274526c4498b80c663e9c83ab55968b4ef4da
monica230 <monicamaltacouto@gmail.com>
2024-10-02 18:00:00 -0300

    commit criando home

---
+++ src/screens/home.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import { homeStyles } from "../styles/home.styles";
+
+export default function Home() {
+    return (
+        <View style={homeStyles.container}>
+            <Text>
+                Home
+            </Text>
+        </View>
+    )
+}
---
+++ src/styles/home.Styles.ts
@@ -0,0 +1,10 @@
+import {StyleSheet} from "react-native"
+
+
+export const homeStyles = StyleSheet.create({
+    container: {
+        flex:1,
+        padding: 20,
+
+    }
+});
